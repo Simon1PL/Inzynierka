97523688b0a0ba961daffc9452173af46e0af1d2
Szynkie <saicxs123@gmail.com>
2021-11-30 15:02:35 +0100

    no message

--- lib/services/programs_service.dart
+++ lib/services/programs_service.dart
@@ -202,5 +202,5 @@ Future<bool> isEnoughFreeSpace(ProgramModel program) async {
 
   var kbNeeded = program.stop!.millisecondsSinceEpoch - (max(program.start!.millisecondsSinceEpoch, DateTime.now().millisecondsSinceEpoch)) / 1000 * (program.channelName!.toLowerCase().contains("hd") ? 4700 : 2900);
 
-  return freeSpace == null ? true : (freeSpace / 128) > kbScheduled + kbNeeded;
+  return freeSpace == null ? true : (freeSpace / 128) > kbScheduled + kbNeeded; // free space [B], needs [kb]
 }
